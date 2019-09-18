d402bfe99e64cbb8ce2bfe015a11c7ef88530c8a
Danny Xie Li <51223285+dnnxl@users.noreply.github.com>
2019-09-17 22:54:07 -0600

    Introduction files

---
+++ Introduction/Definitions functions comments/Definitions-functions-comments.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+#|
+
+Multiline comentary 
+
+|#
+
+; This is a commentary
+; Constants definitions
+
+(define pi 3.14)
+(define phi 1.681)
+(define hola-mundo "hello world")
+
+; Functions or procedures definitions
+
+(define (media a b)
+  (/ (+ a b) 2))
+
